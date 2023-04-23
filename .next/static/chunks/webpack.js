30b757e9ed9ef2503dfcf990e3ac8271cf230122
JohnPonciano <jhow.ponciano@hotmail.com>
2023-04-23 13:49:41 -0300

    Add navbar, and fix Icon

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "68941d6970b83698"; }
+/******/ 		__webpack_require__.h = function() { return "e7b8ded7f2476863"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Navbar from '../navbar'
 import { Box, Container } from '@chakra-ui/react'
 
 
@@ -9,6 +10,7 @@ const Main = ({children, router})=>{
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta name="description" content='Jonathan Ponciano' />
             </Head>
+            <Navbar path={router.asPath}/>
 
             <Container maxW="container.md" pt={14}> 
 
